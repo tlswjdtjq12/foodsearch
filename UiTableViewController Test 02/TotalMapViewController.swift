a94154deeee1c39942749bf3d27b7c2004e985e8
Jonghyun Kim <jhkim@dit.ac.kr>
2018-06-03 19:19:57 +0900

    .

--- UiTableViewController Test 02/TotalMapViewController.swift
+++ UiTableViewController Test 02/TotalMapViewController.swift
@@ -16,6 +16,8 @@ class TotalMapViewController: UIViewController {
     var name:[String] = []
     var type:[String] = []
     var annotations = [MKPointAnnotation]()
+    
+    // name[], type[] 배열 값 받기 위한 인덱스
     var count = 0
     
     override func viewDidLoad() {
@@ -24,44 +26,39 @@ class TotalMapViewController: UIViewController {
     }
     
     func  viewMap(locations:[String], name:[String], type:[String]) {
-        
-                for loc in locations {
-                    print("loc = \(loc)")
-                    let geoCoder = CLGeocoder()
-                    geoCoder.geocodeAddressString(loc , completionHandler: {
-                        (placemarks: [CLPlacemark]?, error: Error?) -> Void in
-                        if let error = error {
-                            print(error)
-                            return
-                        }
-        
-                        if placemarks != nil {
-                            let placemark = placemarks![0]
-        
-                            // Add annotation
-                            let annotation = MKPointAnnotation()
-
-                            if let location = placemark.location {
-                                // Add annotation
-                                annotation.title = self.name[self.count]
-                                annotation.subtitle = self.type[self.count]
-                                
-                                print("title = \(String(describing: annotation.title))")
-                                
-                                self.count = self.count + 1
-                                annotation.coordinate = location.coordinate
-                                self.annotations.append(annotation)
-        
-                                self.totalMapView.addAnnotations(self.annotations)
-        
-                                // Set zoom level
-//                                let region = MKCoordinateRegionMakeWithDistance(annotation.coordinate, 800, 800)
-//                                self.totalMapView.setRegion(region, animated: true)
-                            }
-                        }
-                        self.totalMapView.showAnnotations(self.annotations, animated: true)
-                    })
+        for loc in locations {
+            let geoCoder = CLGeocoder()
+            geoCoder.geocodeAddressString(loc , completionHandler: {
+                (placemarks: [CLPlacemark]?, error: Error?) -> Void in
+                if let error = error {
+                    print(error)
+                    return
                 }
+        
+                if placemarks != nil {
+                    let placemark = placemarks![0]
+                    print(placemarks![0])
+                    
+                    // pin point 을 저장
+                    let annotation = MKPointAnnotation()
+
+                    if let location = placemark.location {
+                        // Add annotation
+                        annotation.title = self.name[self.count]
+                        annotation.subtitle = self.type[self.count]
+                        self.count = self.count + 1
+                        annotation.coordinate = location.coordinate
+                        self.annotations.append(annotation)
+                        self.totalMapView.addAnnotations(self.annotations)
+        
+                        // Set zoom level
+//                              let region = MKCoordinateRegionMakeWithDistance(annotation.coordinate, 800, 800)
+//                              self.totalMapView.setRegion(region, animated: true)
+                    }
+                }
+                self.totalMapView.showAnnotations(self.annotations, animated: true)
+            })
+        }
     }
     
     /*
